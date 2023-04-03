f9aa84261fcb1fa1f6d1359c52397fe49130c21e
MATHEUS WAGATUMA SPADRIZANI <spadrizani.8935@aluno.pr.senac.br>
2023-04-03 08:08:40 -0300

    For Dieimes

---
+++ exemplo_for.php
@@ -0,0 +1,33 @@
+<?php
+
+    //Laço de repetição FOR em PHP
+
+    // Imprima os valores de 1 ao 10 em sua página web
+
+    echo "1";
+    echo "<br>";
+    echo "2";
+    echo "<br>";
+    echo "3";
+    echo "<br>";
+    echo "4";
+    echo "<br>";
+    echo "5";
+    echo "<br>";
+    echo "6";
+    echo "<br>";
+    echo "7";
+    echo "<br>";
+    echo "8";
+    echo "<br>";
+    echo "9";
+    echo "<br>";
+    echo "10";
+    echo "<br>";
+    echo "<br>";
+
+    for ($numero=0; $numero<=10; $numero++) { 
+        echo $numero . "<br>";
+    }
+
+?>
